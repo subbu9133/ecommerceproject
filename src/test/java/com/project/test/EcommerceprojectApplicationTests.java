2e6e151ff7ec36fe8936c2ffa6e626606b7713c0
subbu9133 <subbareddy.0025@gmail.com>
2024-09-30 15:18:31 -0500

    Test file changes

--- src/test/java/com/project/test/EcommerceprojectApplicationTests.java
+++ src/test/java/com/project/test/EcommerceprojectApplicationTests.java
@@ -1,9 +1,10 @@
-package com.example.demo;
+package com.project.test; // Change this line
 
+import com.project.EcommerceprojectApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EcommerceprojectApplication.class)
 class EcommerceprojectApplicationTests {
 
 	@Test
